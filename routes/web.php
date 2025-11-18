360037b94d767b2adca142f9bed992c4fbc9cfb2
ziziii <dfdffdfdd@gmail.com>
2025-11-18 16:03:17 +0800

    hellnah

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('/home');
-}); #default
+}); #default landing page
 
 
 #Route::view('home', 'home'); #this is my first project

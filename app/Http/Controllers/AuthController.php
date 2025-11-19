0ac3accf5cafec77bed448529d92cbcf14e976b0
Ziiziziziz <zuchery@gmail.com>
2025-11-19 20:03:40 +0800

    WEWSSSS

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 class AuthController extends Controller
+
 {
     function showRegister(){
         return view('authentication.register');
@@ -14,12 +17,31 @@ class AuthController extends Controller
         return view('authentication.login');
     }
 
-    function performLogin(){
+    function performLogin(Request $request){
 
     }
 
-    function performRegister(){
+    function performRegister(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:6|confirmed'
+
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->route('login.form')->with('success', 'Registration Successful!');
+
+
         
+    }
+    function login(Request $request){
+
     }
 
 

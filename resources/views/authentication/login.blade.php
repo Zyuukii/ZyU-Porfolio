8064718a48e3df85a5e9d098c7943376c16611eb
ZiiYuu <zucherybuentiempo@gmail.com>
2025-12-06 20:05:23 +0800

    helloluyaa

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -1,20 +1,31 @@
-@include('components.header')
+@include('components.header', ['pageCss' => 'design.css'])
 
-<form action="{{ route('login') }}" method="POST">
+
+<form action="{{ route('login') }}" method="POST" class="form">
     @csrf
-    
-    <label>Email</label>
-    <input type="email" name="email" placeholder="Email" required>
-    
-    <label>Password</label>
-    <input type="password" name="password" placeholder="Password" required>
-    
-    <div style="margin: 10px 0;">
-        <input type="checkbox" name="remember" id="remember">
-        <label for="remember">Remember me</label>
+
+    <div>
+        <h2 class="title">Login</h2>
     </div>
 
-    <input type="submit" value="Login">
+    <div class="flex">
+        <label>
+            <span>Email</span>
+            <input class="input" type="email" name="email" placeholder="Email" required>
+        </label>
+
+        <label>
+            <span>Password</span>
+            <input class="input" type="password" name="password" placeholder="Password" required>
+        </label>
+
+        <div class="checkbox">
+            <input type="checkbox" name="remember" id="remember">
+            <label for="remember" style="margin:0;">Remember me</label>
+        </div>
+
+        <button class="submit" type="submit">Submit</button>
+    </div>
 </form>
 
 @include('components.footer')
--- resources/views/authentication/register.blade.php
+++ resources/views/authentication/register.blade.php
@@ -1,41 +1,47 @@
-@include('components.header')
+@include('components.header', ['pageCss' => 'design.css'])
 
-<form action="{{ route('register')}}" method="POST">
+<form action="{{ route('register')}}" method="POST" class="form">
     @csrf
-    <label>Name</label>
-    <input type="text" 
-    name="name" 
-    placeholder="Full name"
-    >
 
-    <label>Email</label>
-    <input type="email" 
-    name="email" 
-    placeholder="Email"
-    >
+    <div>
+        <h2 class="title">Register</h2>
+    </div>
 
-    <label>Password</label>
-    <input type="password" 
-    name="password" 
-    placeholder="Password"
-    >
+    <div class="flex">
+        <label>
+            <span>Name</span>
+            <input class="input" type="text" name="name" placeholder="Full name" required>
+        </label>
 
-    <label>Retype password</label>
-    <input type="password" 
-    name="password_confirmation" 
-    placeholder="Retype password"
-    >
-    <input type="submit">
+
+        <label>
+            <span>Email</span>
+            <input class="input" type="email" name="email" placeholder="Email" required>
+        </label>
+
+        <label>
+            <span>Password</span>
+            <input class="input" type="password" name="password" placeholder="Password" required>
+        </label>
+
+        <label>
+            <span>Retype password</span>
+            <input class="input" type="password" name="password_confirmation" placeholder="Retype password" required>
+        </label>
+
+        <input type="submit" class="submit">
+    </div>
+    @if ($errors->any())
+        <div class="errors">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li class="alert" role="alert">{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </form>
 
-@if ($errors->any())
-    <div class="errors">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li class ="alert alert-warning" role="alert">{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+
 
 @include('components.footer')
